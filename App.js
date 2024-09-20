e5cbaed0c1bab15638d0851746a3d4b42a19258e
Waterlilyrr <noemict94@hotmail.it>
2024-09-20 18:04:27 +0200

    feat(App): initializes App style, implements goals management logic

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+function goalInputHandler(enteredText) {
+  setEnteredGoalText(enteredText
+
+  )
+};
+function addGoalHandler() {
+  setCourseGoals((currentCourseGoals) => [
+    ...currentCourseGoals, 
+    enteredGoalText
+  ])
+};
+
+
   return (
-    <View style={styles.container}>
-      <Text>Meh!!</Text>
-      <Button title='Tap me!'/>
+    <View style={styles.appContainer}>
+      
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" onChangeText={goalInputHandler} />
+        <Button title="Add Goals" onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((course, i) => <Text key={i}>{course}</Text>)}
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    paddingVertical: 50,
+    paddingHorizontal: 16,
+    gap:10,
+    flex:1
   },
+  inputContainer: {
+    flex: 1,
+   flexDirection: 'row',
+   alignItems:'center',
+    justifyContent: 'space-between',
+    gap:8,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc',
+    marginBottom:20
+  },
+  textInput:{
+    borderWidth: 1, borderColor: '#cccccc', width: '70%', padding:8
+  },
+  goalsContainer: {
+    flex: 5
+  }
 });
